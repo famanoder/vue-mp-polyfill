3e6f2f382f06390c266e65335995a07239154308
famanoder <915905174@qq.com>
2019-04-22 21:30:20 +0800

    featï: optimize computed and add deps collect

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const {getType, defProp} = require('./utils/index');
+const Computed = require('./lib/computed');
 
 function NutComponent(obj) {
   if(getType(Component) !== 'function') return console.error(`no mp 'Component' function.`);
 
   const { behaviors = [], computed, watch } = obj;
+
   const _data = {
     $slot: {
       default: true
     }
   };
- 
+
+  const computedKeys = Object.keys(computed);
+  if(computedKeys.length) {
+    computedKeys.forEach(k => _data[k] = null);
+  }
 
   const behavior = Behavior({
     data: _data,
@@ -36,34 +42,7 @@ function NutComponent(obj) {
       // computed的key：1. 相互依赖；2. 依赖各种data；3. data与computed都有依赖
       // 所以将computed与data合并，重新修改setter，在set中触发computed
       if (computed && getType(computed) === 'object' && Object.keys(computed).length) {
-        const computedKeys = Object.keys(computed);
-        computedKeys.forEach(key => {
-          $this.setData({
-            [key]: ''
-          });
-        });
-        
-        const datas = $this.data;
-        for (const k of Object.keys(datas)) {
-          defProp($this, k, {
-            get() {
-              return $this.data[k];
-            },
-            set(v) {
-              $this.setData({
-                [k]: v
-              });
-              setTimeout(() => {
-                computedKeys.forEach(key => {
-                  $this.setData({
-                    [key]: computed[key].bind($this)()
-                  });
-                });
-              }, 0);
-            }
-          });
-        };
-        
+        Computed(this, computed);
       } 
       if(watch && getType(watch) === 'object' && Object.keys(watch).length) {
         
@@ -78,3 +57,57 @@ function NutComponent(obj) {
 }
 
 module.exports = NutComponent;
+
+// watcher
+// dep
+const data = {
+  a: 12,
+  b: 23,
+  c: 'aa'
+}
+const computeds = {
+  d() {return data.a + data.b;},
+  e() {return data.a + data.c;}
+}
+const deps = {};
+
+function def(obj, key, val) {
+  Object.defineProperty(obj, key, {
+    set(v) {
+      // this.setData({[k]: v});
+      obj[key] = v;
+      val = v;
+      const _deps = deps[key] || [];
+      if(_deps.length) {
+
+      }
+    },
+    get() {
+      const k = deps.target;
+      if(k && !deps[key]) {
+        const keyDeps = deps[key] || [];
+        keyDeps.push(k);
+        deps[key] = keyDeps;
+      }
+      return val;
+    }
+  });
+}
+
+for(let a in data) {
+  def(data, a, data[a]);
+}
+
+function initComputedDeps() {
+  for(let k in computeds) {
+    deps.target = k;
+    deps[k] = [];
+    const v = computeds[k]();
+    // this.setData({
+    //   [k]: v
+    // });
+    delete deps.target;
+  }
+}
+
+initComputedDeps();
